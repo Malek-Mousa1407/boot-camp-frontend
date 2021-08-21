274afc9557195e4c9f8baa583c6c5b33ff363a6d
Malek Mousa <malek.mousa1407@gmail.com>
2021-08-21 13:30:19 +0400

    Configured env vars.

--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -18,39 +18,58 @@ const RegistrationForm = () => {
     let avatarInput;
     let termsAndConditionsCheckbox;
 
-    const register = () => {
+    // Form data is a constructer for creating an object
+    // that works like and HTML form element
+    const formData = new FormData();
 
+    const register = () => {
 
         const errors = [];
         
         // 1. Validate all of the required fields
-        if( firstNameField.value.length === 0 ) {
+        if(firstNameField.value.length === 0) {
             errors.push("Please enter your first name");
         }
-        if( lastNameField.value.length === 0 ) {
+        if(lastNameField.value.length === 0) {
             errors.push("Please enter your last name");
         }
-        if( emailField.value.length === 0 ) {
+        if(emailField.value.length === 0) {
             errors.push("Please enter valid email");
         }
-        if( passwordField.value.length === 0 ) {
+        if(passwordField.value.length === 0) {
             errors.push("Please enter valid password");
         }
-        if( termsAndConditionsCheckbox.checked === false ) {
+        if(termsAndConditionsCheckbox.checked === false) {
             errors.push("Please accept the terms & conditions");
         }
-
         // 1.1 If there are errors, set the state to "validationFailed"
         if(errors.length > 0) {
             setState("validationFailed");
             setErrorsState(errors);
         }
-
         // 1.2 If there are no errors, set the state to "loading"
         else {
             setState("loading");
             setErrorsState([]);
             // 2.1 If the submission is successful, set state to "successful"
+            // Make a fetch request 
+            
+            formData.append('firstName',firstNameField.value);
+            formData.append('lastName',lastNameField.value);
+            formData.append('email',emailField.value);
+            formData.append('password',passwordField.value);
+            formData.append('phoneNumber',phoneNumberField.value);
+
+            fetch(
+                `${process.env.REACT_APP_BACKEND}/users/create`,
+                {
+                    method : 'POST',
+                    body : formData
+                }
+            )
+            .then()
+            .then()
+            .catch();
             // 2.2 If the submission is successful, set state to "unsucessful"
         }
 
